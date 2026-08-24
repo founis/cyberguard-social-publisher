07f1c4c42800b04cfffb54714fc2e443ff5b2e15
founis <founis@gmail.com>
2026-08-24 13:39:06 -0700

    Add includes/class-cgsp-plugin.php

---
+++ includes/class-cgsp-plugin.php
@@ -0,0 +1,48 @@
+<?php
+defined('ABSPATH') || exit;
+
+require_once CGSP_DIR . 'includes/class-cgsp-logger.php';
+require_once CGSP_DIR . 'includes/class-cgsp-social-api.php';
+require_once CGSP_DIR . 'includes/class-cgsp-admin.php';
+
+final class CGSP_Plugin {
+    private static $instance;
+
+    public static function instance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+        add_action('plugins_loaded', array($this, 'load_textdomain'));
+        add_action('cgsp_publish_scheduled_post', array($this, 'publish_scheduled'), 10, 1);
+        if (is_admin()) {
+            new CGSP_Admin();
+        }
+    }
+
+    public function load_textdomain() {
+        load_plugin_textdomain('cyberguard-social-publisher', false, dirname(plugin_basename(CGSP_FILE)) . '/languages');
+    }
+
+    public static function activate() {
+        CGSP_Logger::create_table();
+        add_option('cgsp_settings', array(
+            'graph_version' => 'v23.0',
+            'page_id' => '',
+            'instagram_id' => '',
+            'access_token' => '',
+        ));
+    }
+
+    public static function deactivate() {
+        wp_clear_scheduled_hook('cgsp_publish_scheduled_post');
+    }
+
+    public function publish_scheduled($payload) {
+        $api = new CGSP_Social_API();
+        $api->publish($payload);
+    }
+}
