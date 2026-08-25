ba784390e5cc5c8c88f4bd0501c1f9fe11a410be
founis <founis@gmail.com>
2026-08-24 23:34:39 -0700

    Bump plugin version to 0.3.0

--- cyberguard-social-publisher.php
+++ cyberguard-social-publisher.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CyberGuard Social Publisher
  * Description: Publish and schedule CyberGuard content to the website, Facebook Pages and Instagram Business from one control center.
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author: CyberGuard
  * Requires at least: 6.2
  * Requires PHP: 7.4
@@ -11,7 +11,7 @@
 
 defined('ABSPATH') || exit;
 
-define('CGSP_VERSION', '0.2.0');
+define('CGSP_VERSION', '0.3.0');
 define('CGSP_FILE', __FILE__);
 define('CGSP_DIR', plugin_dir_path(__FILE__));
 define('CGSP_URL', plugin_dir_url(__FILE__));
